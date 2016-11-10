50af3753310b8850a7c66c495f89eab92d1f599f
smomel <smomel@gmail.com>
2016-11-10 23:15:29 +0300

    10.11.2016

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    /*$('.bxslider').bxSlider({
+        mode: 'fade',
+        speed: 2000,
+        easing: 'ease-in-out',
+        pager: false,
+        controls: false,
+        auto: true,
+        pause: 10000,
+    });*/
+});
